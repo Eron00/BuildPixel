93b60ce03593951ecafc38de62e73b967173c2de
Eron00 <eron.silva@fatec.sp.gov.br>
2017-07-14 02:49:53 -0300

    Retirada da logica que esperava um array bidimensional como parametro. O que ocasionava um grande numero de loops e um grande uso da função "malloc" para alocação de memória, comprometendo o desempenho.

--- app/src/main/jni/ProcessamentoUnario.c
+++ app/src/main/jni/ProcessamentoUnario.c
@@ -9,113 +9,97 @@
 #include <math.h>
 
 JNIEXPORT void JNICALL Java_com_aplicacao_Modelo_NDK_processamentoUnario(JNIEnv *env, jobject instance, jobject Foto, jstring cNomeProcesso) {
-    int i, j;
 
-    float **Red   = (float **) (malloc(linha * sizeof(float *)));
-    float **Blue  = (float **) (malloc(linha * sizeof(float *)));
-    float **Green = (float **) (malloc(linha * sizeof(float *)));
-
-    for (i = 0; i < linha; i++) {
-        Red[i]   = (float *) (malloc(coluna * sizeof(float)));
-        Blue[i]  = (float *) (malloc(coluna * sizeof(float)));
-        Green[i] = (float *) (malloc(coluna * sizeof(float)));
-    }
+    AndroidBitmapInfo dadosImagem;
+    void* localPixels;
+    int linha, coluna, red,green,blue;
+    u_int32_t* pixel;
 
 
-    for (i = 0; i < linha; i++) {
-        for (j = 0; j < coluna; j++) {
-            Red[i][j] = 0;
-            Green[i][j] = 0;
-            Blue[i][j] = 0;
-        }
-    }
+    AndroidBitmap_getInfo(env,Foto,&dadosImagem);
+    AndroidBitmap_lockPixels(env,Foto,&localPixels);
 
-    jint *valorPixel;
-    valorPixel = (*env)->GetIntArrayElements(env, FotoResultante, NULL);
-    int *pixel = valorPixel;
-
-    int indice = 0;
-    for (i = 0; i < linha; i++) {
-        for (j = 0; j < coluna; j++, indice += 3) {
-            Red[i][j] = pixel[indice];
-            Green[i][j] = pixel[indice + 1];
-            Blue[i][j] = pixel[indice + 2];
-        }
-    }
 
     const char *cNomeProcessamento =  (*env)->GetStringUTFChars(env, cNomeProcesso, 0);
-    for (i = 0; i < linha; i++) {
-        for (j = 0; j < coluna; j++) {
+
+    for(linha = 0; linha < dadosImagem.height; linha++){
+        pixel = (uint32_t*)localPixels;                            //carregando a próxima linha de pixels da imagem
+        for(coluna =0; coluna < dadosImagem.width; coluna++){
+
             int Pixel = 0;
+            //extração dos dados RGB do pixel
+            blue  = (int)((pixel[coluna] & 0x00FF0000) >> 16);
+            green = (int)((pixel[coluna] & 0x0000FF00) >> 8);
+            red   = (int) (pixel[coluna] & 0x00000FF );
+
             if (strcmp(cNomeProcessamento, "Amarelo") == 0) {
-                Red[i][j] = 255;
-                Green[i][j] = 255;
+                red = 255;
+                green = 255;
             }
             if (strcmp(cNomeProcessamento, "Azul") == 0) {
-                Red[i][j] = 0;
-                Green[i][j] = 0;
+                red = 0;
+                green = 0;
             }
             if (strcmp(cNomeProcessamento, "Blue") == 0) {
-                Red[i][j] = Blue[i][j];
-                Green[i][j] = Blue[i][j];
+                red = blue;
+                green = blue;
 
             }
             if (strcmp(cNomeProcessamento, "Brilho") == 0) {
 
-                Pixel = (int)(fmax(Red[i][j], fmax(Green[i][j],Blue[i][j])) +
-                             (fmin(Red[i][j], fmin(Green[i][j],Blue[i][j])/2.0)));
-
-                Red[i][j] = Pixel ;
-                Green[i][j] = Pixel;
-                Blue[i][j] = Pixel;
+                Pixel = (int)(fmax(red, fmax(green,blue)) +
+                              (fmin(red, fmin(green,blue)/2.0)));
+                red = Pixel ;
+                green = Pixel;
+                blue = Pixel;
 
             }
             if (strcmp(cNomeProcessamento, "Ciano") == 0) {
 
-                Green[i][j] = 255;
-                Blue[i][j] = 255;
+                green = 255;
+                blue = 255;
             }
             if (strcmp(cNomeProcessamento, "CinzaMedia") == 0) {
 
-                Pixel = (int)(Red[i][j] + Green[i][j] + Blue[i][j]) / 3;
+                Pixel = (int)(red + green +blue) / 3;
 
-                Red[i][j] = Pixel ;
-                Green[i][j] = Pixel;
-                Blue[i][j] = Pixel;
+                red = Pixel ;
+                green = Pixel;
+                blue = Pixel;
             }
             if (strcmp(cNomeProcessamento, "Green") == 0) {
-                Red[i][j] = Green[i][j];
-                Blue[i][j] = Green[i][j];
+                red = green;
+                blue = green;
             }
 
             if (strcmp(cNomeProcessamento, "Inverter") == 0) {
                 int PixelR = 0;
                 int PixelG = 0;
                 int PixelB = 0;
-                PixelR = (int) (255 - Red[i][j]);
-                PixelG = (int) (255 - Green[i][j]);
-                PixelB = (int) (255 - Blue[i][j]);
-                Red[i][j] = PixelR;
-                Green[i][j] = PixelG;
-                Blue[i][j] = PixelB;
+                PixelR = (255 - red);
+                PixelG = (255 - green);
+                PixelB = (255 - blue);
+                red = PixelR;
+                green = PixelG;
+                blue = PixelB;
             }
 
             if (strcmp(cNomeProcessamento, "Luminosidade") == 0) {
 
-                Pixel  = (int) (0.2125 * Red[i][j] + 0.7154 * Green[i][j]+ 0.0721 * Blue[i][j]);
-                Red[i][j] = Pixel;
-                Green[i][j] = Pixel;
-                Blue[i][j] = Pixel;
+                Pixel  = (int) (0.2125 * red + 0.7154 * green + 0.0721 * blue);
+                red = Pixel;
+                green = Pixel;
+                blue = Pixel;
             }
 
             if (strcmp(cNomeProcessamento, "Magenta") == 0) {
-                Red[i][j] = 255;
-                Blue[i][j] = 255;
+                red = 255;
+                blue = 255;
             }
 
             if (strcmp(cNomeProcessamento, "Red") == 0) {
-                Green[i][j] = Red[i][j];
-                Blue[i][j] = Red[i][j];
+                green = red;
+                blue  = red;
             }
 
             if (strcmp(cNomeProcessamento, "Sepia") == 0) {
@@ -123,7 +107,7 @@ JNIEXPORT void JNICALL Java_com_aplicacao_Modelo_NDK_processamentoUnario(JNIEnv 
                 int PixelG = 0;
                 int PixelB = 0;
 
-                Pixel = (int) ((Red[i][j] * 0.3) + (Green[i][j] * 0.59) + (Blue[i][j] * 0.11));
+                Pixel = (int) ((red * 0.3) + (green* 0.59) + (blue* 0.11));
 
 
                 PixelR = Pixel + 40;
@@ -138,51 +122,45 @@ JNIEXPORT void JNICALL Java_com_aplicacao_Modelo_NDK_processamentoUnario(JNIEnv 
                 if(PixelB < 0)
                     PixelB = 0;
 
-                Red[i][j]   = PixelR;
-                Green[i][j] = PixelG;
-                Blue[i][j]  = PixelB;
+                red   = PixelR;
+                green = PixelG;
+                blue  = PixelB;
             }
 
             if (strcmp(cNomeProcessamento, "Threshold") == 0) {
-                if(Red[i][j] > 200)
+                if(red > 200)
                 {
-                    Red[i][j] = 255;
-                    Green[i][j] = 255;
-                    Blue[i][j] = 255;
+                    red = 255;
+                    green = 255;
+                    blue = 255;
                 }
                 else
                 {
-                    Red[i][j] = 0;
-                    Green[i][j] = 0;
-                    Blue[i][j] = 0;
+                    red = 0;
+                   green = 0;
+                   blue = 0;
                 }
             }
             if (strcmp(cNomeProcessamento, "Verde") == 0) {
-                Red[i][j] = 0;
-                Blue[i][j] = 0;
+                red = 0;
+                blue = 0;
             }
             if (strcmp(cNomeProcessamento, "Vermelho") == 0) {
 
-                Green[i][j] = 0;
-                Blue[i][j] = 0;
+                green = 0;
+                blue = 0;
             }
+
+            // atribuindo novos valores ao pixel
+            pixel[coluna] = (uint32_t) (((blue << 16) & 0x00FF0000) |
+                                        ((green << 8) & 0x0000FF00) |
+                                        (red          & 0x000000FF));
         }
+
+        localPixels = (char*)localPixels + dadosImagem.stride;    //pulando para a proxima linha da imagem
     }
 
-    indice = 0;
-    for (i = 0; i < linha; i++) {
-        for (j = 0; j < coluna; j++, indice += 3) {
-            pixel[indice] =     (int) Red[i][j];
-            pixel[indice + 1] = (int) Green[i][j];
-            pixel[indice + 2] = (int) Blue[i][j];
-        }
-    }
-    (*env)->ReleaseIntArrayElements(env, FotoResultante, valorPixel, 0);
-
-    free(Red);
-    free(Green);
-    free(Blue);
-
+    AndroidBitmap_unlockPixels(env,Foto);
 }
 
 void processamentoUnario(JNIEnv *env, jobject instance, jintArray FotoResultante, jint linha, jint coluna, jstring cNomeProcesso){
