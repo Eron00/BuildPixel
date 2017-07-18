3cfafde45a6eee31ed548b001d4833748d408cb2
Eron00 <eron.silva@fatec.sp.gov.br>
2017-07-18 04:22:56 -0300

    Média calculada com sucesso. Foi necessário extrair os dados para uma estrutura de imagem auxiliar e a partir dela foi possível calcular e remontar os pixels sem bugs e distorções na imagem

--- app/src/main/jni/Media.c
+++ app/src/main/jni/Media.c
@@ -1,6 +1,7 @@
 
 #include <jni.h>
 #include <android/bitmap.h>
+#include <malloc.h>
 #include "zoaFotoNativeLib.h"
 
 /*******************************************************************************************************
@@ -32,99 +33,80 @@ JNIEXPORT void JNICALL Java_com_aplicacao_Modelo_NDK_media(JNIEnv *env, jobject 
 
     AndroidBitmapInfo dadosImagem;                                  //Estrutura nativa dos dados da Imagem(Altura, largura,etc)
     void *localPixels;                                              //ponteiro para referenciar os pixels da Imagem
-    int coluna, linha, red, green, blue,alpha;
+    int coluna, linha;
     uint32_t *pixel;
 
-
-
-    AndroidBitmap_getInfo(env, foto, &dadosImagem);                 //capturando as informações da imagem
     AndroidBitmap_lockPixels(env, foto, &localPixels);              //capturando os dados dos pixels e bloqueando acesso ao local da memoria da imagem
+    AndroidBitmap_getInfo(env, foto, &dadosImagem);                 //capturando as informações da imagem
+    ARGB pixelTemp[dadosImagem.height][dadosImagem.width];          //struct da imagem
 
-    ARGB pixelTemp[dadosImagem.height][dadosImagem.width];
+    for(linha = 0; linha < dadosImagem.height; linha++){
+        pixel = (uint32_t*)localPixels;                            //carregando a próxima linha de pixels da imagem
+        for(coluna =0; coluna < dadosImagem.width; coluna++){
 
-    for(linha = 1; linha < (dadosImagem.height-1); linha++){         //inicializando para começar na segunda linha, para não haver acesso indevido de memória
-        pixel = (uint32_t*)localPixels;
-        for(coluna = 1; coluna < (dadosImagem.width-1); coluna++){    //inicializando para começar na segunda coluna, para não haver acesso indevido de memória
             //extração dos dados RGB do pixel
-//**************************************************************************************************
-            alpha  = (int)((pixel[coluna] & 0xFF000000) >> 24);
-
-            //capturando os valores do azul
-            //noroeste
-            blue =(int) ((pixel[(linha-1)*dadosImagem.width+(coluna-1)] & 0x00FF0000) >> 16);
-            //norte
-            blue+=(int) ((pixel[(linha-1)*dadosImagem.width+(coluna)]   & 0x00FF0000) >> 16);
-            //nordeste
-            blue+=(int) ((pixel[(linha-1)*dadosImagem.width+(coluna+1)] & 0x00FF0000) >> 16);
-            //leste
-            blue+=(int) ((pixel[(linha)*dadosImagem.width+(coluna-1)]   & 0x00FF0000) >> 16);
-            //centro
-            blue+=(int) ((pixel[(linha)*dadosImagem.width+(coluna)]     & 0x00FF0000) >> 16);
-            //oeste
-            blue+=(int) ((pixel[(linha)*dadosImagem.width+(coluna+1)]   & 0x00FF0000) >> 16);
-            //sudoeste
-            blue+=(int) ((pixel[(linha+1)*dadosImagem.width+(coluna-1)] & 0x00FF0000) >> 16);
-            //sul
-            blue+=(int) ((pixel[(linha+1)*dadosImagem.width+(coluna)]   & 0x00FF0000) >> 16);
-            //sudeste
-            blue+=(int) ((pixel[(linha+1)*dadosImagem.width+(coluna+1)] & 0x00FF0000) >> 16);
-//******************************************************************************************************
-            //capturando os valores do verde
-            //noroeste
-            green =(int) ((pixel[(linha-1)*dadosImagem.width+(coluna-1)] & 0x0000FF00) >> 8);
-            //norte
-            green+=(int) ((pixel[(linha-1)*dadosImagem.width+(coluna)]   & 0x0000FF00) >> 8);
-            //nordeste
-            green+=(int) ((pixel[(linha-1)*dadosImagem.width+(coluna+1)] & 0x0000FF00) >> 8);
-            //leste
-            green+=(int) ((pixel[(linha)*dadosImagem.width+(coluna-1)]   & 0x0000FF00) >> 8);
-            //centro
-            green+=(int) ((pixel[(linha)*dadosImagem.width+(coluna)]     & 0x0000FF00) >> 8);
-            //oeste
-            green+=(int) ((pixel[(linha)*dadosImagem.width+(coluna+1)]   & 0x0000FF00) >> 8);
-            //sudoeste
-            green+=(int) ((pixel[(linha+1)*dadosImagem.width+(coluna-1)] & 0x0000FF00) >> 8);
-            //sul
-            green+=(int) ((pixel[(linha+1)*dadosImagem.width+(coluna)]   & 0x0000FF00) >> 8);
-            //sudeste
-            green+=(int) ((pixel[(linha+1)*dadosImagem.width+(coluna+1)] & 0x0000FF00) >> 8);
-
-//*********************************************************************************************************************
-            //capturando os valores do vermelho
-            //noroeste
-            red =(int) (pixel[(linha-1)*dadosImagem.width+(coluna-1)]  & 0x000000FF);
-            //norte
-            red+=(int) (pixel[(linha-1)*dadosImagem.width+(coluna)]    & 0x000000FF);
-            //nordeste
-            red+=(int) (pixel[(linha-1)*dadosImagem.width+(coluna+1)]  & 0x000000FF);
-            //leste
-            red+=(int) (pixel[(linha)*dadosImagem.width+(coluna-1)]    & 0x000000FF);
-            //centro
-            red+=(int) (pixel[(linha)*dadosImagem.width+(coluna)]      & 0x000000FF);
-            //oeste
-            red+=(int) (pixel[(linha)*dadosImagem.width+(coluna+1)]    & 0x000000FF);
-            //sudoeste
-            red+=(int) (pixel[(linha+1)*dadosImagem.width+(coluna-1)]  & 0x000000FF);
-            //sul
-            red+=(int) (pixel[(linha+1)*dadosImagem.width+(coluna)]    & 0x000000FF);
-            //sudeste
-            red+=(int) (pixel[(linha+1)*dadosImagem.width+(coluna+1)]  & 0x000000FF);
-
-            blue/=9;
-            red/=9;
-            green/=9;
-            
-            // atribuindo novos valores ao pixel
-            pixel[coluna] = (uint32_t)  ((alpha << 24)& 0xFF000000) |
-                                        ((blue << 16) & 0x00FF0000) |
-                                        ((green << 8) & 0x0000FF00) |
-                                        (red          & 0x000000FF);
+            pixelTemp[linha][coluna].blue  = (uint8_t) ((pixel[coluna] & 0x00FF0000) >> 16);
+            pixelTemp[linha][coluna].green = (uint8_t) ((pixel[coluna] & 0x0000FF00) >> 8);
+            pixelTemp[linha][coluna].red   = (uint8_t)  (pixel[coluna] & 0x00000FF );
         }
-        localPixels = (uint32_t*)localPixels + dadosImagem.stride;    //pulando para a proxima linha da imagem
+        localPixels = (char *)localPixels + dadosImagem.stride;    //pulando para a proxima linha da imagem
     }
 
-    AndroidBitmap_unlockPixels(env,foto);                         //liberando a memoria alocada para a imagem
+    for(linha = 1; linha < (dadosImagem.height-1); linha++){          //inicializando para começar na segunda linha, para não haver acesso indevido de memória
+        for(coluna = 1; coluna < (dadosImagem.width-1); coluna++){    //inicializando para começar na segunda coluna, para não haver acesso indevido de memória
+
+            //calculando os valores da média do azul
+                                pixelTemp[linha  ][coluna  ].blue =
+                    (uint8_t) ((pixelTemp[linha-1][coluna-1].blue + 	  //noroeste
+                                pixelTemp[linha-1][coluna  ].blue + 	  //norte
+                                pixelTemp[linha-1][coluna+1].blue + 	  //nordeste
+                                pixelTemp[linha  ][coluna-1].blue + 	  //leste
+                                pixelTemp[linha  ][coluna  ].blue + 	  //centro
+                                pixelTemp[linha  ][coluna+1].blue + 	  //oeste
+                                pixelTemp[linha+1][coluna-1].blue + 	  //sudoeste
+                                pixelTemp[linha+1][coluna  ].blue + 	  //sul
+                                pixelTemp[linha+1][coluna+1].blue) / 9);  //sudeste
+
+            //calculando os valores da média do verde
+                                pixelTemp[linha  ][coluna  ].green =
+                    (uint8_t) ((pixelTemp[linha-1][coluna-1].green + 	   //noroeste
+                                pixelTemp[linha-1][coluna  ].green + 	   //norte
+                                pixelTemp[linha-1][coluna+1].green + 	   //nordeste
+                                pixelTemp[linha  ][coluna-1].green + 	   //leste
+                                pixelTemp[linha  ][coluna  ].green + 	   //centro
+                                pixelTemp[linha  ][coluna+1].green + 	   //oeste
+                                pixelTemp[linha+1][coluna-1].green + 	   //sudoeste
+                                pixelTemp[linha+1][coluna  ].green + 	   //sul
+                                pixelTemp[linha+1][coluna+1].green) / 9);  //sudeste
+
+            //calculando os valores da média do vermelho
+                                pixelTemp[linha  ][coluna  ].red  =
+                    (uint8_t) ((pixelTemp[linha-1][coluna-1].red + 	     //noroeste
+                                pixelTemp[linha-1][coluna  ].red + 	     //norte
+                                pixelTemp[linha-1][coluna+1].red + 	     //nordeste
+                                pixelTemp[linha  ][coluna-1].red + 	     //leste
+                                pixelTemp[linha  ][coluna  ].red + 	     //centro
+                                pixelTemp[linha  ][coluna+1].red + 	     //oeste
+                                pixelTemp[linha+1][coluna-1].red + 	     //sudoeste
+                                pixelTemp[linha+1][coluna  ].red + 	     //sul
+                                pixelTemp[linha+1][coluna+1].red) / 9);  //sudeste
+        }
+
+    }
+
+    for(linha = 0; linha < dadosImagem.height; linha++){
+        pixel = (uint32_t*)localPixels;                            //carregando a próxima linha de pixels da imagem
+        for(coluna =0; coluna < dadosImagem.width; coluna++){
+
+            //remontando o pixel com os valores processados
+            pixel[coluna] = (uint32_t)
+                            ((pixelTemp[linha][coluna].blue  << 16) & 0x00FF0000) |
+                            ((pixelTemp[linha][coluna].green << 8)  & 0x0000FF00) |
+                            ( pixelTemp[linha][coluna].red          & 0x000000FF);
+        }
+        localPixels = (char *)localPixels + dadosImagem.stride;    //pulando para a proxima linha da imagem
+    }
+    AndroidBitmap_unlockPixels(env,foto);                          //liberando a memoria alocada para a imagem
+
 }
-
-
 
