d576f3bb344d94cf0f172f4ef2b0a19b4022e8b8
Eron00 <eron.silva@fatec.sp.gov.br>
2017-08-20 03:55:46 -0300

    limpeza de código comentado

--- app/src/main/java/com/aplicacao/Processamento/ProcessamentoArea/ErrorDiffusion.java
+++ app/src/main/java/com/aplicacao/Processamento/ProcessamentoArea/ErrorDiffusion.java
@@ -26,44 +26,6 @@ public class ErrorDiffusion implements OperacoesArea {
 
             ndk.errorDifusion(Foto);
 
-            /*
-            int linha  = CalcularFoto.getLinha();
-            int coluna = CalcularFoto.getColuna();
-
-            int PropagedError;
-            int TotalError;
-
-            PropagedError = 0;
-            TotalError = 0;
-
-            Imagem ImagemResultante = new Imagem(linha,coluna);
-            Imagem PreImagem = new Imagem(linha,coluna);
-
-            for (int i = 0; i < linha; i++) {
-                for (int j = 0; j < coluna; j++) {
-
-                    int Pixel = (int)(( CalcularFoto.getR(i, j) + CalcularFoto.getG(i, j) + CalcularFoto.getB(i, j))/3d);
-                    PreImagem.setRGB(i, j, Pixel, Pixel, Pixel);
-                }
-            }
-            for (int i = 1; i < linha; i++) {
-                for (int j = 1; j < coluna; j++) {
-
-                    TotalError = PreImagem.getR(i,j) + PropagedError;
-
-                    if(TotalError > 127)
-                        ImagemResultante.setRGB(i, j, 255, 255, 255);
-                    else
-                        ImagemResultante.setRGB(i, j, 0, 0, 0);
-
-                    PropagedError = TotalError - ImagemResultante.getR(i,j);
-                }
-            }
-
-            CalcularFoto.setImagem(ImagemResultante.getImagem());
-        }
-        */
-
         }
     }
 
