f2a26c5507e520a10e21d42185b2a67e8bd62168
Eron00 <eron.silva@fatec.sp.gov.br>
2018-02-14 22:57:50 -0300

    adaptação da função pra processar o bitmap de forma nativa.

--- app/src/main/jni/Convolucao.c
+++ app/src/main/jni/Convolucao.c
@@ -94,15 +94,8 @@ JNIEXPORT void Java_com_aplicacao_Modelo_NDK_convolucao(JNIEnv *env, jobject ins
     AndroidBitmap_unlockPixels(env,foto);                         //liberando a memoria alocada para a imagem
 }
 
+void convolucao(JNIEnv *env, jobject instance,jobject foto, char *mascara, int linhaMascara, int colunaMascara){
 
-
-
-
-
-
-
-
-void convolucao(JNIEnv *env, jobject instance,jobject foto, jstring mascara, jint linhaMascara, jint colunaMascara){
     AndroidBitmapInfo dadosImagem;                                  //Estrutura nativa dos dados da Imagem(Altura, largura,etc)
     void *localPixels;                                              //ponteiro para referenciar os pixels da Imagem
     int coluna, linha;
@@ -111,20 +104,29 @@ void convolucao(JNIEnv *env, jobject instance,jobject foto, jstring mascara, jin
     AndroidBitmap_getInfo(env, foto, &dadosImagem);                 //capturando as informações da imagem
     AndroidBitmap_lockPixels(env, foto, &localPixels);              //capturando os dados dos pixels e bloqueando acesso ao local da memoria da imagem
 
-    struct ARGB pixelTemp[dadosImagem.height][dadosImagem.width];
+    int **Red   = (int **) (malloc(dadosImagem.height * sizeof(int *)));
+    int **Blue  = (int **) (malloc(dadosImagem.height * sizeof(int *)));
+    int **Green = (int **) (malloc(dadosImagem.height * sizeof(int *)));
 
+    for (linha = 0; linha < dadosImagem.height; linha++) {
+        Red  [linha] = (int *) (malloc(dadosImagem.width * sizeof(int)));
+        Blue [linha] = (int *) (malloc(dadosImagem.width * sizeof(int)));
+        Green[linha] = (int *) (malloc(dadosImagem.width * sizeof(int)));
+    }
 
     for(linha = 0; linha < dadosImagem.height; linha++){
         pixel = (uint32_t*)localPixels;                            //carregando a próxima linha de pixels da imagem
         for(coluna =0; coluna < dadosImagem.width; coluna++){
 
             //extração dos dados RGB do pixel
-            pixelTemp[linha][coluna].blue  = (uint8_t) ((pixel[coluna] & 0x00FF0000) >> 16);
-            pixelTemp[linha][coluna].green = (uint8_t) ((pixel[coluna] & 0x0000FF00) >> 8);
-            pixelTemp[linha][coluna].red   = (uint8_t) (pixel[coluna] & 0x00000FF );
+            Blue [linha][coluna] = (int) ((pixel[coluna] & 0x00FF0000) >> 16);
+            Green[linha][coluna] = (int) ((pixel[coluna] & 0x0000FF00) >> 8);
+            Red  [linha][coluna] = (int)  (pixel[coluna] & 0x00000FF );
         }
         localPixels = (char*)localPixels + dadosImagem.stride;    //pulando para a proxima linha da imagem
     }
+    AndroidBitmap_unlockPixels(env,foto);
+
 
     // para encontrar posicionamento da mascara na coordenada linha
     int linhaMask  = (linhaMascara - 1) / 2;
@@ -133,13 +135,16 @@ void convolucao(JNIEnv *env, jobject instance,jobject foto, jstring mascara, jin
 
     int iLinha, jColuna, i, j, iMascara,jMascara;
 
-    const char *cNomeMascara = (*env)->GetStringUTFChars(env, mascara, 0);
+
     float dMascaraFiltro[linhaMascara][colunaMascara];
-    PopularMascara(cNomeMascara, linhaMascara, colunaMascara , dMascaraFiltro);
+    PopularMascara(mascara, linhaMascara, colunaMascara , dMascaraFiltro);
 
+    AndroidBitmap_getInfo(env, foto, &dadosImagem);                 //capturando as informações da imagem
+    AndroidBitmap_lockPixels(env, foto, &localPixels);              //capturando os dados dos pixels e bloqueando acesso ao local da memoria da imagem
 
-    for (iLinha = 0; iLinha < dadosImagem.width; iLinha++) {
-        for (jColuna = 0; jColuna < dadosImagem.height; jColuna++) {
+    for (iLinha = 0; iLinha < dadosImagem.height; iLinha++) {
+        pixel = (uint32_t*)localPixels;
+        for (jColuna = 0; jColuna < dadosImagem.width; jColuna++) {
 
             float somatoria[] = {0, 0, 0};
 
@@ -147,43 +152,31 @@ void convolucao(JNIEnv *env, jobject instance,jobject foto, jstring mascara, jin
                 for (j = jColuna - colunaMask, jMascara = 0; j <= jColuna + colunaMask; j++, jMascara++) {
 
                     // verificar se está entre  0 e a largura da imagem
-                    if ((i >= 0) && (i < dadosImagem.width) && (j >= 0) && (j < dadosImagem.height)) {
-                        somatoria[0] += pixelTemp[i][j].blue  * dMascaraFiltro[iMascara][jMascara];
-                        somatoria[1] += pixelTemp[i][j].green * dMascaraFiltro[iMascara][jMascara];
-                        somatoria[2] += pixelTemp[i][j].red   * dMascaraFiltro[iMascara][jMascara];
+                    if ((i >= 0) && (i < dadosImagem.height) && (j >= 0) && (j < dadosImagem.width)) {
+                        somatoria[0] += Blue [i][j] * dMascaraFiltro[iMascara][jMascara];
+                        somatoria[1] += Green[i][j] * dMascaraFiltro[iMascara][jMascara];
+                        somatoria[2] += Red  [i][j] * dMascaraFiltro[iMascara][jMascara];
                     }
                 }
-
-                if (somatoria[0] > 255) { somatoria[0] = 255; }
-                else if (somatoria[0] < 0) { somatoria[0] = 0; }
-
-                if (somatoria[1] > 255) { somatoria[1] = 255; }
-                else if (somatoria[1] < 0) { somatoria[1] = 0; }
-
-                if (somatoria[2] > 255) { somatoria[2] = 255; }
-                else if (somatoria[2] < 0) { somatoria[2] = 0; }
-
-                pixelTemp[i][j].blue  = (uint8_t) somatoria[0];
-                pixelTemp[i][j].green = (uint8_t) somatoria[1];
-                pixelTemp[i][j].red   = (uint8_t) somatoria[2];
             }
+            if (somatoria[0] > 255) { somatoria[0] = 255; }
+            else if (somatoria[0] < 0) { somatoria[0] = 0; }
+
+            if (somatoria[1] > 255) { somatoria[1] = 255; }
+            else if (somatoria[1] < 0) { somatoria[1] = 0; }
+
+            if (somatoria[2] > 255) { somatoria[2] = 255; }
+            else if (somatoria[2] < 0) { somatoria[2] = 0; }
+
+            pixel[jColuna] = (uint32_t) (((int)somatoria[0] << 16) & 0x00FF0000) |
+                             (((int)somatoria[1] << 8)  & 0x0000FF00) |
+                             ( (int)somatoria[2]        & 0x000000FF);
 
         }
-    }
-
-    for(linha = 0; linha < dadosImagem.height; linha++){
-        pixel = (uint32_t*)localPixels;                            //carregando a próxima linha de pixels da imagem
-        for(coluna =0; coluna < dadosImagem.width; coluna++){
-
-            //extração dos dados RGB do pixel
-
-            pixel[coluna] = (uint32_t) (((pixelTemp[linha][coluna].blue << 16) & 0x00FF0000) |
-                                        ((pixelTemp[linha][coluna].green << 8) & 0x0000FF00) |
-                                        ( pixelTemp[linha][coluna].red         & 0x000000FF));
-        }
-
         localPixels = (char*)localPixels + dadosImagem.stride;    //pulando para a proxima linha da imagem
     }
-
+    free(Red);
+    free(Green);
+    free(Blue);
     AndroidBitmap_unlockPixels(env,foto);                         //liberando a memoria alocada para a imagem
 }
