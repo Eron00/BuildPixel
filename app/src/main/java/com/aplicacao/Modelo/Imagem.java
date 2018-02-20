9437e69e3b5bf99aa3af07a8cd1db2a190aa5eb1
Eron00 <eron.silva@fatec.sp.gov.br>
2018-02-20 13:55:45 -0300

    Inclusão dos métodos: Redimensionar - para adaptar a imagem a uma proporção aceitável para processamento Rotacionar - método privado para rotacionar a imagem em 90 graus a direita VerificaOrientacaoTelaImagem - Verificação da tela do dispositivo. Alguns aparelhos ao criar o arquivo da imagem mudam a orientação para horizontal e este método rotaciona a imagem de acordo. CopiaPathOriginal - envia o path da imagem original para recuperação. Atualiza imagem - para a atualização do imageView que abriga a foto. Necessita da Activity corrente.

--- app/src/main/java/com/aplicacao/Modelo/Imagem.java
+++ app/src/main/java/com/aplicacao/Modelo/Imagem.java
@@ -1,10 +1,16 @@
 package com.aplicacao.Modelo;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
+import android.media.Image;
 import android.widget.ImageView;
+
+import com.aplicacao.eron.Activities.ProcessamentoActivity;
+import com.aplicacao.eron.build_pixel.R;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -19,9 +25,7 @@ public class Imagem {
     private String Path;
     private String PathImagemOriginal;
 
-    public Imagem(Bitmap foto){
-        this.imagem = foto;
-    }
+
     public Imagem(){
     }
 
@@ -33,23 +37,103 @@ public class Imagem {
         Path = path;
     }
 
-    public String getPathImagemOriginal() {
+    private String getPathImagemOriginal() {
         return PathImagemOriginal;
     }
 
     public void setPathImagemOriginal(String pathImagemOriginal) {
         PathImagemOriginal = pathImagemOriginal;
     }
+
     public Bitmap getImagem() {
         return imagem;
     }
+
     public void setImagem(Bitmap imagem) {
         this.imagem = imagem;
     }
+
     public int getLinha(){
         return(this.imagem.getWidth());
     }
+
     public int getColuna(){
         return(this.imagem.getHeight());
     }
+
+    public void VerificaOrientacaoTelaImagem() {
+        try {
+            ExifInterface ei = new ExifInterface(getPath());
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_UNDEFINED);
+
+            Bitmap rotatedBitmap = null;
+            switch (orientation) {
+
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    setImagem(RotacionarImagem(imagem, 90));
+                    break;
+
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    setImagem(RotacionarImagem(imagem, 180));
+                    break;
+
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    setImagem(RotacionarImagem(imagem, 270));
+                    break;
+
+                case ExifInterface.ORIENTATION_NORMAL:
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void Redimensionar() {
+        BitmapFactory.Options bmpOptions = new BitmapFactory.Options();
+        bmpOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(getPath(), bmpOptions);
+        bmpOptions.inSampleSize = 7;
+        bmpOptions.inJustDecodeBounds = false;
+        imagem = BitmapFactory.decodeFile(getPath(), bmpOptions);
+
+    }
+
+    private Bitmap RotacionarImagem (Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
+    public void CopiaPathOriginal(){
+        setPath(getPathImagemOriginal());
+    }
+
+    public void AtualizaImagem(Activity activity) {
+        ImageView imgView = activity.findViewById(R.id.imageView);
+        File file = null;
+
+        Bitmap FotoProcessada = getImagem();
+        Matrix matrix = new Matrix();
+
+        FotoProcessada = Bitmap.createBitmap(FotoProcessada, 0, 0,
+               getLinha(),
+               getColuna(), matrix, true);
+
+        try {
+            Ferramentas tool = new Ferramentas();
+            file = tool.CriarArquivo();
+            FileOutputStream fos = new FileOutputStream(file);
+            FotoProcessada.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        imgView.setImageBitmap(FotoProcessada);
+        setPath(file.getAbsolutePath());
+    }
+
 }
